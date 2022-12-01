5d913e66cb2f5715b518d7983ba5514af02518f3
Leandro Lopez <leandro230599@outlook.com>
2022-12-01 19:48:18 -0300

    Agregado eje 2 python

---
+++ Python/Eje2.py
@@ -0,0 +1,6 @@
+# Modifica la variable del anterior ejercicio en la consola de python y después muestrala por consola para ver la modificación de la variable.
+
+hola = "Hola mundo!"
+hola = input()
+
+print(hola)
